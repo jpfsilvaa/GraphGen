d04419c136870aa80007ed3bc5cfc1351e1fb70d
jps <j263792@dav.unicamp.br>
2023-01-05 11:34:48 +0100

    utils folder with a file code for treating the json input

---
+++ utils/json_utils.py
@@ -0,0 +1,39 @@
+import json, math
+from classes.Cloudlet import Cloudlet
+from classes.Resources import Resources
+from classes.User import UserVM
+
+def readJsonInput(jsonFilePath):
+    jsonFile = open(jsonFilePath)
+    data = json.load(jsonFile)
+    jsonFile.close()
+    return data
+
+def buildCloudlets(jsonData):
+    cloudlets = []
+    for cloudlet in jsonData:
+        cloudlets.append(Cloudlet(str(cloudlet['cId']),
+                            str(cloudlet['node']),
+                            int(cloudlet['coverageArea']),
+                            Resources(int(cloudlet['c_CPU']), 
+                            int(cloudlet['c_RAM']),
+                            int(cloudlet['c_storage']))
+                            )
+                        )
+    return cloudlets
+
+def buildUserVms(jsonData):
+    vmsList = []
+    for user in jsonData:
+        vmsList.append(UserVM(str(user['vId']),
+                            str(user['vmType']),
+                            int(user['bid']),
+                            int(user['avgSpeed']),
+                            user['initialPos'],
+                            user['route'],
+                            Resources(int(user['v_CPU']), 
+                            int(user['v_RAM']),
+                            int(user['v_storage']))
+                            )
+                        )
+    return vmsList
